6096f71ead33dce3ce17704ef8b66823e07690a5
AdityaBare <bareaditya0@gmail.com>
2025-07-13 19:14:14 +0530

    cros setup

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://viveo-1.onrender.com/"  // ✅ or your real frontend domain
+}));
+
 app.use(bodyParser.json());
 
 // Serve frontend HTML
